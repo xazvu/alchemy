f07a76bf7d7e8b25b144bbd5690cb99e19040b33
Ahmedik <isa732827@gmail.com>
2025-06-07 01:02:15 +0300

    Создал еще одну таблицу сделал связь один к одному и написал crud запросы

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import String, Integer
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import String, Integer, ForeignKey
 
 class Base(DeclarativeBase):
     pass
@@ -10,3 +10,17 @@ class User(Base):
     name: Mapped[str] = mapped_column(String(20))
     city: Mapped[str] = mapped_column(String(20))
     age: Mapped[int] = mapped_column(Integer)
+
+    passport: Mapped["Passport"] = relationship(back_populates='user', uselist=False, cascade="all, delete-orphan")
+
+
+class Passport(Base):
+    __tablename__ = 'passports'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'), unique=True)
+
+    number: Mapped[int] = mapped_column(Integer, unique=True)
+    user: Mapped['User'] = relationship(back_populates='passport')
+
+
